d66b570e7623353e408471790352b8cabd259790
shashi.yatindra <shashi.yatindra@gmail.com>
2019-03-08 14:48:14 +0100

    test change

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ os.chdir('static')
 
 httpd = Server(("", PORT), Handler)
 try:
-  print("Start serving at port %i" % PORT)
+  print("Start pythonserving at port %i" % PORT)
   httpd.serve_forever()
 except KeyboardInterrupt:
   pass
